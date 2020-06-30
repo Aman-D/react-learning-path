dbcad84abf2c3d356ddb1be055388a109b2f2883
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-06-30 13:30:33 +0530

    Convert to JSX

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,11 @@
 import React from "react";
-export default function Pet(props) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
+
+export default function Pet({ name, animal, breed }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h1>{animal}</h1>
+      <h1>{breed}</h1>
+    </div>
+  );
 }
